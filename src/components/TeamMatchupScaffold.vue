6c1a3a7a6a4f60edb883f68d9ba983f87944f9ed
Piero del Valle <devapeu@gmail.com>
2026-01-06 12:45:16 -0500

    update ui

--- src/components/TeamMatchupScaffold.vue
+++ src/components/TeamMatchupScaffold.vue
@@ -20,39 +20,99 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="teams__wrapper" style="margin-bottom: 0!important" :class="thumbnail ? 'thumbnail' : ''">
-    <div class="teams__team">
-      <div class="teams__header">
-        <div class="teams__name">Equipo 1</div>
-        <div class="teams__header-side">
-          {{ team1.score }}
-        </div>
+  <div class="team-matchup">
+    <div class="team-matchup__header">
+      <div class="team-matchup__labels">
+        <span>{{ team1.label }}</span>
+        <span>{{ team2.label }}</span>
       </div>
-      <div class="team-box">
-        <PlayerBadge
-          v-for="p in team1.players"
-          :key="p.id" 
-          :player="p" />
+      <div class="team-matchup__probability">
+        <div 
+          class="team-matchup__probability-item" 
+          :style="{ 
+            width: team1.probability + '%',
+            backgroundColor: team1.probability > 50 ? '#10b981' : '#ef4444'
+          }">
+          {{ team1.probability }}%
+        </div>
+        <div 
+          class="team-matchup__probability-item left" 
+          :style="{ 
+            width: team2.probability + '%',
+            backgroundColor: team2.probability > 50 ? '#10b981' : '#ef4444'
+          }">
+          {{ team2.probability }}%
+        </div>
       </div>
     </div>
-    <div class="teams__team">
-      <div class="teams__header teams__header--inverse">
-        <div class="teams__name">Equipo 2</div>
-        <div class="teams__header-side">
-          {{ team2.score }}
+    <div class="teams__wrapper" style="margin-bottom: 0!important" :class="thumbnail ? 'thumbnail' : ''">
+      <div class="teams__team">
+        <div class="teams__header">
+          <div class="teams__score">{{ team1.score }}</div>
+          <div class="teams__header-side">
+            <div class="wins">{{ team1.wins }}</div>
+          </div>
+        </div>
+        <div class="team-box">
+          <PlayerBadge
+            v-for="p in team1.players"
+            :key="p.id" 
+            :player="p" />
         </div>
       </div>
-      <div class="team-box">
-        <PlayerBadge
-          v-for="p in team2.players"
-          :key="p.id" 
-          :player="p" />
+      <div class="teams__team">
+        <div class="teams__header teams__header--inverse">
+          <div class="teams__score">{{ team2.score }}</div>
+          <div class="teams__header-side">
+            <div class="wins">{{ team2.wins }}</div>
+          </div>
+        </div>
+        <div class="team-box">
+          <PlayerBadge
+            v-for="p in team2.players"
+            :key="p.id" 
+            :player="p" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="sass" scoped>
+
+.team-matchup
+  &__header
+    padding-top: 6px
+    padding-left: 6px
+    padding-right: 6px
+  &__labels
+    display: flex
+    font-size: 10px
+    justify-content: space-between
+  &__probability
+    width: 100%
+    display: flex
+    margin-top: 1px
+    border-radius: 2px
+    overflow: hidden
+    &-item
+      display: block
+      background: red
+      font-size: 4px
+      padding-top: 1px
+      padding-left: 2px
+      &.left
+        text-align: right
+        padding-right: 2px
+
+.wins
+  background: #161005
+  width: 16px
+  height: 16px
+  display: flex
+  align-items: center
+  justify-content: center
+  border-radius: 2px
 .thumbnail
   &.teams__wrapper
     padding: 6px
@@ -64,12 +124,16 @@ const props = defineProps({
   .teams__name
     font-size: 9px
   .teams__header-side
+    display: flex
+    gap: 4px
     height: 16px
   .teams__team
-    width: 120px
+    width: 100%
     min-width: unset
     padding: 2px
     gap: 2px
+  .teams__score
+    font-size: 10px
   :deep(.player)
     height: 21px
     font-size: 8px
