28e245b4cd46d2d325aa6187fd76be370beed634
Piero del Valle <devapeu@gmail.com>
2026-06-13 19:21:51 -0400

    add header link

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <header class="header">
     <div class="header__wrapper">
-      <h1>Age of Mythology - Auto Balancer 3000</h1>
-      <nav class="header__nav" style="display: none;">
-        <router-link to="/">Inicio</router-link>
-        <router-link to="/stats">Stats</router-link>
+      <h1>
+        <router-link to="/">Age of Mythology - Auto Balancer 3000</router-link>
+      </h1>
+      <nav class="header__nav">
+        <router-link to="/stats">Estadísticas</router-link>
       </nav>
     </div>
   </header>
