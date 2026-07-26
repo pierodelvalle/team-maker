1f74469be4df1f580fb094815d225cbdef19d6c9
Piero del Valle <devapeu@gmail.com>
2026-07-26 13:16:39 -0400

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <header class="header">
     <div class="header__wrapper">
-      <h1>
-        <router-link to="/">Age of Mythology - Auto Balancer 3000</router-link>
-      </h1>
+      <div class="header__brand">
+        <router-link to="/" class="header__logo-link">
+          <img src="/logo.png" alt="GG Army" class="header__logo" />
+        </router-link>
+      </div>
       <nav class="header__nav">
         <router-link to="/matches">Partidas</router-link>
         <router-link to="/stats">Estadísticas</router-link>
